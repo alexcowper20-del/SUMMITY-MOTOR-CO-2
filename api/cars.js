48b97336d583f8ea5712dc9013ee9c0f5420d090
alexcowper20-del <alexcowper20@gmail.com>
2026-03-26 17:44:07 +0000

    Update cars.js

--- api/cars.js
+++ api/cars.js
@@ -36,7 +36,7 @@ export default async function handler(req, res) {
         : []
     }));
 
-    res.status(200).json(rows);
+    res.status(200).json(data);
 
   } catch (err) {
     res.status(500).json({ error: err.message });
